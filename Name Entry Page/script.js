605dcde6771c896b7e5f8d0d8c4749ff463161a7
root <root@LAPTOP-M8O94PKH>
2023-11-23 19:43:14 +0900

    HTML/CSSのみ作成。iconはローカルのまま。リンク先未設定。

---
+++ Name Entry Page/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+function registerUser() {
+  // ステップ1: 入力する情報を取得（ここではユーザー名）
+  const name = document.getElementById('name-input').value;
+
+  // ステップ2: POSTに送信先URLを指定（PHPファイルのパス）
+  if (name) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'register.php', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    // ステップ4: 結果を確認
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+        console.log(xhr.responseText); // レスポンスをコンソールに出力
+        if (xhr.status == 200) {
+          alert('登録が成功しました！');
+        } else {
+          alert('登録中にエラーが発生しました。');
+        }
+      }
+    };
+
+    // ステップ3: PHPにデータを送信
+    xhr.send('name=' + encodeURIComponent(name));
+  } else {
+    alert('名前を入力してください。');
+  }
+}
